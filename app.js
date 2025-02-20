79b48977038bf585fb54afd1497b127c770cfea7
pink-clarinet65 <conniemutua@gmail.com>
2025-02-20 16:21:01 +0300

    added ids to remaining items on weather description and updated them on app.js to reflect real time weather information.

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ function updateCurrentWeather(response) {
   let cityElement = document.querySelector("#city-name");
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+  let descriptionElement = document.querySelector("#weather-description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#weather-humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  let windElement = document.querySelector("#weather-wind");
+  windElement.innerHTML = response.data.wind.speed;
+  console.log(response.data);
+  console.log(response.data.wind.speed);
+  console.log(response.data.condition.description);
   console.log(response.data.temperature.current);
 }
 
