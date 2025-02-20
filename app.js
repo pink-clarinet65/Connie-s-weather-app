cf074214aaa5d8c22424a07d7182b3cbf4a77b77
pink-clarinet65 <conniemutua@gmail.com>
2025-02-20 17:30:12 +0300

    added time and date element to app.js and included formatDate function

--- app.js
+++ app.js
@@ -10,12 +10,30 @@ function updateCurrentWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   let windElement = document.querySelector("#weather-wind");
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  let timeElement = document.querySelector("#current-time");
+  let date = new Date(response.data.time * 1000);
+  timeElement.innerHTML = formatDate(date);
   console.log(response.data);
-  console.log(response.data.wind.speed);
-  console.log(response.data.condition.description);
-  console.log(response.data.temperature.current);
 }
 
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
 function searchCity(city) {
   let apiKey = "7482b6ta250748o436a7585c26ecf30b";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
