28b38ea7fe1216e3f2085e2cd0f429ef81e738a5
pink-clarinet65 <conniemutua@gmail.com>
2025-02-20 15:36:51 +0300

    Added two functions in app.js for API integration to update weather in real time.

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+function updateCurrentWeather(response) {
+  console.log(response.data);
+}
+
+function searchCity(city) {
+  let apiKey = "7482b6ta250748o436a7585c26ecf30b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateCurrentWeather);
+}
+
 function search(event) {
   event.preventDefault();
   let searchFormInputElement = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city-name");
   cityElement.innerHTML = searchFormInputElement.value;
+  searchCity(searchFormInputElement.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
